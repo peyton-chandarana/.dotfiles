f54cb5f9f290fce78f1ff3ef9602cbe9e5d71942
Peyton Chandarana <peytonchandarana@gmail.com>
2025-11-22 18:16:52 -0500

    Removed old stuff.

--- etc/startwslvpnsetup.ps1
+++
@@ -1,2 +0,0 @@
-start-process powershell -verb runas -ArgumentList '-ExecutionPolicy Bypass -F "\\wsl.localhost\Debian\home\s7117\.dotfiles\etc\vpnsetupwsl.ps1"'
-
--- etc/vpnsetupwsl.ps1
+++
@@ -1,4 +0,0 @@
-Get-NetAdapter | Where-Object {$_.InterfaceDescription -Match "Cisco AnyConnect"} | Set-NetIPInterface -InterfaceMetric 4000
-Get-NetIPInterface -InterfaceAlias "vEthernet (WSL)" | Set-NetIPInterface -InterfaceMetric 1
-Read-Host "Press enter to exit..."
-
