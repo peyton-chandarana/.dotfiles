811b9e1774a4796dd5ec2864a66c4038f66407e7
Peyton Chandarana <peytonchandarana@gmail.com>
2026-02-11 12:06:26 -0500

    Added remote server configuration.

---
+++ bin/remote_ubuntu.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+################################################################################
+CURR_DIR=$(pwd)
+CURR_OS=$(uname)
+################################################################################
+# Sanity check
+if [[ "$CURR_OS" != *"Linux"* ]]; then
+    echo "ERROR --> Incorrect OS detected for this target!"
+    exit
+fi
+################################################################################
+# Save old zshrc if one exists
+if [[ -f "$HOME/.zshrc" ]]; then
+    echo "LOG --> Found existing .zshrc file! Saving backup!"
+    mkdir $HOME/.zsh_bups
+    cp ~/.zshrc ~/.zsh_bups/.bup.zshrc
+    # Delete old .zshrc
+    rm "$HOME/.zshrc"
+fi
+################################################################################
+echo "LOG --> Creating directories..."
+mkdir -m 700 -p ~/.cli_tools
+mkdir -m 700 -p ~/.ssh
+################################################################################
+# Move config files around.
+cp ./etc/config ~/.ssh
+cp ./etc/vimrc ~/.vimrc
+################################################################################
+# Install dependencies
+echo "LOG --> Installing dependencies and updating..."
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install zsh build-essential wget curl vim htop -y
+################################################################################
+# Configure Zsh
+echo "LOG --> Configuring Zsh..."
+chsh -s $(which zsh)
+echo "LOG --> Configuring Zsh Settings..."
+echo "export HISTFILE=~/.zsh_history" >> ~/.zshrc
+echo "export HISTSIZE=1000000000" >> ~/.zshrc
+echo "export SAVEHIST=1000000000" >> ~/.zshrc
+echo 'export HISTTIMEFORMAT="[%F %T] "' >> ~/.zshrc
+echo "setopt INC_APPEND_HISTORY" >> ~/.zshrc
+echo "setopt EXTENDED_HISTORY" >> ~/.zshrc
+echo "setopt HIST_IGNORE_ALL_DUPS" >> ~/.zshrc
+echo "autoload -Uz compinit && compinit" >> ~/.zshrc
+echo "zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'" >> ~/.zshrc
+echo 'source ~/.dotfiles/etc/zshrc_custom' >> ~/.zshrc
+################################################################################
+# Install and Configure Oh-My-Posh.
+echo "LOG --> Installing Oh-My-Posh..."
+echo "export XDG_CACHE_HOME=~/.xdg-cache" >> ~/.zshrc
+echo 'export PATH=$PATH:~/.oh-my-posh/bin' >> ~/.zshrc
+mkdir ~/.oh-my-posh
+mkdir ~/.oh-my-posh/bin
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O ~/.oh-my-posh/bin/oh-my-posh
+chmod +x ~/.oh-my-posh/bin/oh-my-posh
+echo "LOG --> Setting Oh-My-Posh Theme..."
+echo 'eval "$(oh-my-posh init zsh --config ~/.dotfiles/etc/s7117.omp.json)"' >> ~/.zshrc
+################################################################################
+# Install CLI tools.
+## Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.cli_tools/zsh-autosuggestions
+echo "source ~/.cli_tools/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+## Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.cli_tools/zsh-syntax-highlighting
+echo "source ~/.cli_tools/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+################################################################################
+# Post Run Instructions
+echo "DONE"
